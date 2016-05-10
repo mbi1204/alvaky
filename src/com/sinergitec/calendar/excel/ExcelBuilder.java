5b85a371568ea708db66105a47891895ed996e84
Alex181945 <alestra55@hotmail.com>
2016-05-10 13:23:23 -0500

    OK

--- src/com/sinergitec/calendar/excel/ExcelBuilder.java
+++ src/com/sinergitec/calendar/excel/ExcelBuilder.java
@@ -41,13 +41,15 @@ public class ExcelBuilder extends AbstractExcelView {
         style.setFillPattern(CellStyle.SOLID_FOREGROUND);
         font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
         font.setColor(HSSFColor.WHITE.index);
+        //font.setFontHeight((short)24);
         style.setFont(font);
+        style.setAlignment(style.ALIGN_CENTER);
          
         // create header row
         HSSFRow header = sheet.createRow(0);
          
-        /*Row row = sheet.createRow((short) 1);
-        Cell cell = row.createCell((short) 1);*/
+        //Row row = sheet.createRow((short) 1);
+        //Cell cell = row.createCell((short) 1);
         
         sheet.addMergedRegion(CellRangeAddress.valueOf("A1:A2"));
         sheet.addMergedRegion(CellRangeAddress.valueOf("B1:E1"));
@@ -63,6 +65,7 @@ public class ExcelBuilder extends AbstractExcelView {
         header.getCell(0).setCellStyle(style);
          
         header.createCell(1).setCellValue("Agua Cruda");
+        sheet.createRow(1).createCell(1).setCellValue("Chlorine (Cloro) 0.2 - 1.5 ppm");
         header.getCell(1).setCellStyle(style);
          
         header.createCell(5).setCellValue("Agua Suavizada");
@@ -88,15 +91,35 @@ public class ExcelBuilder extends AbstractExcelView {
         
          
         // create data rows
-        int rowCount = 1;
+        int rowCount = 2;
          
         for (InfEjecutivo aBook : listBooks) {
             HSSFRow aRow = sheet.createRow(rowCount++);
-            aRow.createCell(0).setCellValue(aBook.getcSucursal());
-            aRow.createCell(1).setCellValue(aBook.getcNomSuc());
-            aRow.createCell(2).setCellValue(aBook.getcAcciones());
-            aRow.createCell(3).setCellValue(aBook.getDtFechaV());
-            aRow.createCell(4).setCellValue(aBook.getcRecomienda());
+            aRow.createCell(0).setCellValue(aBook.getDtFechaV());
+            aRow.createCell(1).setCellValue(aBook.getDeCloroEnt());
+            aRow.createCell(2).setCellValue(aBook.getDePHEnt());
+            aRow.createCell(3).setCellValue(aBook.getDeDurEnt());
+            aRow.createCell(4).setCellValue(aBook.getDeSTDEnt());
+            aRow.createCell(5).setCellValue(aBook.getDeCloroFilt());
+            aRow.createCell(6).setCellValue(aBook.getDePHFilt());
+            aRow.createCell(7).setCellValue(aBook.getDeDurFilt());
+            aRow.createCell(8).setCellValue(aBook.getDeSTDFilt());
+            aRow.createCell(9).setCellValue(aBook.getDeCloroUV());
+            aRow.createCell(10).setCellValue(aBook.getDePHUV());
+            aRow.createCell(11).setCellValue(aBook.getDeDurUV());
+            aRow.createCell(12).setCellValue(aBook.getDeSTDUV());
+            aRow.createCell(13).setCellValue(aBook.getDeAlcUV());
+            aRow.createCell(14).setCellValue(aBook.getDeConsSed());
+            aRow.createCell(15).setCellValue(aBook.getDeConsSal());
+            aRow.createCell(16).setCellValue(aBook.getDeConsCarb());
+            aRow.createCell(17).setCellValue(aBook.getDeConsSalP());
+            aRow.createCell(18).setCellValue(aBook.getcOtRefacc());
+            aRow.createCell(19).setCellValue(aBook.getDeORCant());
+            aRow.createCell(20).setCellValue(aBook.getcAcciones());
+            aRow.createCell(21).setCellValue(aBook.getcRecomienda());
+            aRow.createCell(22).setCellValue(aBook.getcHoraEntrada());
+            aRow.createCell(23).setCellValue(aBook.getcHoraSalida());
+            aRow.createCell(24).setCellValue(aBook.getcTiempoTot());
         }
     }
 }
