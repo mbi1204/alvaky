fffaa09d86eb18635e854d2324b6afcd9d6fa747
Alex181945 <alestra55@hotmail.com>
2016-07-12 15:25:49 -0500

    OK

--- src/com/dhtmlx/demoapp/controller/ReporteController.java
+++ src/com/dhtmlx/demoapp/controller/ReporteController.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 
-import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
