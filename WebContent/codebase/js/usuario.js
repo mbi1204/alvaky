7718b477563d7cc2f878386af601abc11af798df
alexs <alexs@Alex>
2016-07-21 15:43:57 -0500

    OK

--- WebContent/codebase/js/usuario.js
+++ WebContent/codebase/js/usuario.js
@@ -1,47 +1,66 @@
 /**
  * Autor: Estrada Mares Alejandro Fecha: 11 de Abril de 2016 Programa: reporte
- * Descripcion: Genera la comunicacion con el server para el llenado de
- * la tabla de usuarios web
+ * Descripcion: Genera la comunicacion con el server para el llenado de la tabla
+ * de usuarios web
  */
 
-function listadoUsuarios(){
-	$.ajax({
-		type : "GET",
-		url : "UsuarioListado",
-		dataType : "json",
-		contentType : "application/json",
-		data : {
-			lActivo : "TRUE"
-		},
-		success : function(data) {
-			
-			arreglo = data;
-			
-			if(data != ""){
-				$("#mytable > tbody").empty();
-				var contador = 0;
-				for ( var item in data) {
+function listadoUsuarios() {
+	$
+			.ajax({
+				type : "GET",
+				url : "UsuarioListado",
+				dataType : "json",
+				contentType : "application/json",
+				data : {
+					lActivo : "TRUE"
+				},
+				success : function(data) {
 
-					var activo = data[item].lActivo ? "Activo":"Desactivo";
-					$('#mytable > tbody').append('<tr class="text-center">'                 +
-							'<td class="text-center">'  + data[item].cNombre                +   '</td>' +
-							'<td class="text-center">'  + data[item].cUsuarioWeb            +   '</td>' + 
-							'<td class="text-center">'	+ data[item].ctUsuaCompWeb.cCveCia  +   '</td>' +
-							'<td class="text-center">'	+ data[item].cCliente               +   '</td>' +
-							'<td class="text-center">'	+ activo                            +   '</td>' +
-							'<td class="text-center"><input type=\"checkbox\" id=\"registro\" name=\"registro\" value="'+contador+'" /></td>'+'</tr>');
-					contador++;
-					}	
-				
-				}else{
-					swal("No Existen Registros");
+					arreglo = data;
+
+					if (data != "") {
+						$("#mytable > tbody").empty();
+						var contador = 0;
+						for ( var item in data) {
+
+							var activo = data[item].lActivo ? "Activo"
+									: "Desactivo";
+							$('#mytable > tbody')
+									.append(
+											'<tr class="text-center">'
+													+ '<td class="text-center">'
+													+ data[item].cNombre
+													+ '</td>'
+													+ '<td class="text-center">'
+													+ data[item].cUsuarioWeb
+													+ '</td>'
+													+ '<td class="text-center">'
+													+ data[item].ctUsuaCompWeb.cCveCia
+													+ '</td>'
+													+ '<td class="text-center">'
+													+ data[item].cCliente
+													+ '</td>'
+													+ '<td class="text-center">'
+													+ activo
+													+ '</td>'
+													+ '<td class="text-center"><input type=\"checkbox\" id=\"registro\" name=\"registro\" value="'
+													+ contador + '" /></td>'
+													+ '</tr>');
+							contador++;
+						}
+
+					} else {
+						swal("No Existen Registros");
+					}
+				},
+				error : function(data, status, error) {
+					sweetAlert(
+							"Oops...",
+							"Algo salio mal intenta mas tarde o contacta a sistemas",
+							"error");
 				}
-		},
-		error : function(data,status,error) {
-			sweetAlert("Oops...", "Algo salio mal intenta mas tarde o contacta a sistemas", "error");
-		}
-		
-	});
+
+			});
 }
 
 function add_ctUsuarioWeb() {
@@ -62,48 +81,48 @@ function edit_ctUsuario(cUsuario) {
 		swal("Selecciona al menos un registro")
 	} else {
 		var json = JSON.stringify(lista);
-		
-		/*$.get("UsuarioGet",json, function(result) {
-			$("#UpdateCtUsuario_Dialog").html(result);
-			$("#UpdateCtUsuario_Dialog").dialog("option", "title",
-					'Editar Usuario');
-			$("#UpdateCtUsuario_Dialog").dialog('open');
-		});*/
-		
-		
-		$.ajax({
-			type : "GET",
-			url : "UsuarioGet",
-			dataType : "json",
-			contentType : "application/json",
-			data : {
-				listUsuarios : json
-			},
-			success : function(data) {
 
-				if (data != null && data != "") {
-					
-					$.get("UsuarioModificar", function(result) {
-					$("#UpdateCtUsuario_Dialog").html(result);
-					$("#UpdateCtUsuario_Dialog").dialog("option", "title",
-							'Editar Usuario');
-					$("#UpdateCtUsuario_Dialog").dialog('open');
-					});
-						
+		/*
+		 * $.get("UsuarioGet",json, function(result) {
+		 * $("#UpdateCtUsuario_Dialog").html(result);
+		 * $("#UpdateCtUsuario_Dialog").dialog("option", "title", 'Editar
+		 * Usuario'); $("#UpdateCtUsuario_Dialog").dialog('open'); });
+		 */
 
-				} else {
-					//swal("Exito!", "Registro Eliminado", "success");
-					sweetAlert("Oops...", data, "error");
-				}
+		$
+				.ajax({
+					type : "GET",
+					url : "UsuarioGet",
+					dataType : "json",
+					contentType : "application/json",
+					data : {
+						listUsuarios : json
+					},
+					success : function(data) {
 
-			},
-			error : function(data, status, error) {
-				sweetAlert("Oops...",
-						"Algo salio mal intenta mas tarde o contacta a sistemas",
-						"error");
-			}
+						if (data != null && data != "") {
 
-		});
+							$.get("UsuarioModificar", function(result) {
+								$("#UpdateCtUsuario_Dialog").html(result);
+								$("#UpdateCtUsuario_Dialog").dialog("option",
+										"title", 'Editar Usuario');
+								$("#UpdateCtUsuario_Dialog").dialog('open');
+							});
+
+						} else {
+							// swal("Exito!", "Registro Eliminado", "success");
+							sweetAlert("Oops...", data, "error");
+						}
+
+					},
+					error : function(data, status, error) {
+						sweetAlert(
+								"Oops...",
+								"Algo salio mal intenta mas tarde o contacta a sistemas",
+								"error");
+					}
+
+				});
 	}
 }
 
@@ -122,83 +141,101 @@ function resetDialog(form) {
 }
 
 function cambiarDisplay(id) {
-	if (!document.getElementById){
+	if (!document.getElementById) {
 		return false;
 	}
 	var fila = document.getElementById(id);
 	if (fila.style.display != "none") {
 		fila.style.display = "none"; // ocultar fila
-	} 
+	}
 }
 
-function busquedaCompania(){
-	
-	$.ajax({
-		type : "GET",
-		url : "CompaniaListado",
-		dataType : "json",
-		contentType : "application/json",
-		data : {
-			lActivos : "TRUE"
-		},
-		success : function(data) {
-			
-			if(data != ""){
-				
-				$('#cCompania option').remove();
-				$('#cCompania').append('<option selected=\"selected\">Seleccione una compa&ntilde;ia:</option>');
-				
-				for ( var item in data) {
-					$('#cCompania').append('<option value="'+data[item].cCveCia+'">'+data[item].cRazonS+'</option>');
-					}	
-				
-				}else{
-					swal("No Existen Registros");
+function busquedaCompania() {
+
+	$
+			.ajax({
+				type : "GET",
+				url : "CompaniaListado",
+				dataType : "json",
+				contentType : "application/json",
+				data : {
+					lActivos : "TRUE"
+				},
+				success : function(data) {
+
+					if (data != "") {
+
+						$('#cCompania option').remove();
+						$('#cCompania')
+								.append(
+										'<option selected=\"selected\">Seleccione una compa&ntilde;ia:</option>');
+
+						for ( var item in data) {
+							$('#cCompania').append(
+									'<option value="' + data[item].cCveCia
+											+ '">' + data[item].cRazonS
+											+ '</option>');
+						}
+
+					} else {
+						swal("No Existen Registros");
+					}
+				},
+				error : function(data, status, error) {
+					sweetAlert(
+							"Oops...",
+							"Algo salio mal intenta mas tarde o contacta a sistemas",
+							"error");
 				}
-		},
-		error : function(data,status,error) {
-			sweetAlert("Oops...", "Algo salio mal intenta mas tarde o contacta a sistemas", "error");
-		}
-		
-	});
-	
+
+			});
+
 }
 
-function busquedaCliente(){
+function busquedaCliente() {
 	var cCompania = $('#cCompania').val();
-	
+
 	var fila = document.getElementById("cliente");
 	fila.style.display = "";
-	
-	$.ajax({
-		type : "GET",
-		url : "ClienteListado",
-		dataType : "json",
-		contentType : "application/json",
-		data : {
-			cCveCia : cCompania
-		},
-		success : function(data) {
-			
-			if(data != ""){
-				
-				$('#Cliente option').remove();
-				$('#Cliente').append('<option selected=\"selected\">Seleccione un cliente:</option>');
-				
-				for ( var item in data) {
-					$('#Cliente').append('<option value="'+data[item].cCliente+'">'+data[item].cRazonS+'</option>');
-					}	
-				
-				}else{
-					swal("No Existen Registros");
+
+	$
+			.ajax({
+				type : "GET",
+				url : "ClienteListado",
+				dataType : "json",
+				contentType : "application/json",
+				data : {
+					cCveCia : cCompania
+				},
+				success : function(data) {
+
+					if (data != "") {
+
+						$('#Cliente option').remove();
+						$('#Cliente')
+								.append(
+										'<option selected=\"selected\">Seleccione un cliente:</option>');
+
+						for ( var item in data) {
+							$('#Cliente').append(
+									'<option value="' + data[item].cCliente
+											+ '">' + data[item].cRazonS
+											+ '</option>');
+						}
+
+					} else {
+						swal("No Existen Registros");
+					}
+				},
+				error : function(data, status, error) {
+					sweetAlert(
+							"Oops...",
+							"Algo salio mal intenta mas tarde o contacta a sistemas",
+							"error");
 				}
-		},
-		error : function(data,status,error) {
-			sweetAlert("Oops...", "Algo salio mal intenta mas tarde o contacta a sistemas", "error");
-		}
-		
-	});
-	
+
+			});
+
 }
 
 function leerCookies(galleta) {
@@ -219,65 +256,73 @@ function borraRegistro() {
 	$('#registro:checked').each(function() {
 		lista.push(arreglo[$(this).val()]);
 	});
-	
-	if(lista == null || lista == ""){
+
+	if (lista == null || lista == "") {
 		swal("Selecciona al menos un registro")
-	}else{
+	} else {
 		console.log(lista);
-		swal({
-			title : "Estas seguro?",
-			text : "Si borras el registro no podras recuperarlo!",
-			type : "warning",
-			showCancelButton : true,
-			confirmButtonColor : "#DD6B55",
-			confirmButtonText : "Si, Borralo!",
-			closeOnConfirm : false}, function(){
-		
-		var json = JSON.stringify(lista);
+		swal(
+				{
+					title : "Estas seguro?",
+					text : "Si borras el registro no podras recuperarlo!",
+					type : "warning",
+					showCancelButton : true,
+					confirmButtonColor : "#DD6B55",
+					confirmButtonText : "Si, Borralo!",
+					closeOnConfirm : false
+				},
+				function() {
 
-		$.ajax({
-			type : "GET",
-			url : "UsuarioEliminar",
-			dataType : "json",
-			contentType : "application/json",
-			data : {
-				listUsuarios : json
-			},
-			success : function(data) {
+					var json = JSON.stringify(lista);
 
-				if (data != null && data != "") {
+					$
+							.ajax({
+								type : "GET",
+								url : "UsuarioEliminar",
+								dataType : "json",
+								contentType : "application/json",
+								data : {
+									listUsuarios : json
+								},
+								success : function(data) {
 
-					sweetAlert("Oops...", data, "error");
+									if (data != null && data != "") {
 
-				} else {
-					swal("Exito!", "Registro Eliminado", "success");
-					location.href = "usuario";
-				}
+										sweetAlert("Oops...", data, "error");
 
-			},
-			error : function(data, status, error) {
-				sweetAlert("Oops...",
-						"Algo salio mal intenta mas tarde o contacta a sistemas",
-						"error");
-			}
+									} else {
+										swal("Exito!", "Registro Eliminado",
+												"success");
+										location.href = "usuario";
+									}
 
-		});
-		swal("Deleted!", "Your imaginary file has been deleted.", "success"); });
+								},
+								error : function(data, status, error) {
+									sweetAlert(
+											"Oops...",
+											"Algo salio mal intenta mas tarde o contacta a sistemas",
+											"error");
+								}
+
+							});
+					swal("Deleted!", "Your imaginary file has been deleted.",
+							"success");
+				});
 	}
 }
 
 $(document).ready(function() {
-	
+
 	var mensajeError = document.getElementById("mensajeError").innerHTML;
-	if(mensajeError != null && mensajeError != ""){
+	if (mensajeError != null && mensajeError != "") {
 		sweetAlert("Oops...", mensajeError, "error");
 	}
-	
+
 	listadoUsuarios();
 	busquedaCompania();
 	cambiarDisplay('cliente');
 	cambiarDisplay('estatus');
-	
+
 	$('#AddCtUsuarioWeb_Dialog').dialog({
 
 		autoOpen : false,
@@ -288,7 +333,7 @@ $(document).ready(function() {
 		buttons : {
 			"Guardar" : function() {
 				$('#Form_ctUsuarioWeb_Add').submit();
-				
+
 			},
 			"Cancelar" : function() {
 				$(this).dialog('close');
@@ -302,7 +347,6 @@ $(document).ready(function() {
 		}
 	});
 
-	
 	$('#UpdateCtUsuario_Dialog').dialog({
 
 		autoOpen : false,
@@ -326,4 +370,3 @@ $(document).ready(function() {
 		}
 	});
 });
-
