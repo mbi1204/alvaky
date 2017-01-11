f69ebed85c28c3875435fad4c1f65fb5c5d2eb32
Alex181945 <alestra55@hotmail.com>
2017-01-11 14:54:46 -0600

    OK

--- WebContent/codebase/js/incidencias.js
+++ WebContent/codebase/js/incidencias.js
@@ -96,7 +96,7 @@ function detalle(valor,lectura){
 			$("#myTable > tbody").empty();
 			
 			var sucursal = "";
-			var contador = 0;
+			var contador = 1;
 			
 			$('#myTable > thead').append('<th class="text-center"> Tiendas </th>');
 			$('#myTable > thead').append('<th class="text-center"> Cantidad </th>');
@@ -107,13 +107,16 @@ function detalle(valor,lectura){
 				
 				if (datos.calidadParam[item].cTienda3 != sucursal){
 					
-					contador = 1;
 					sucursal = datos.calidadParam[item].cTienda3;
 					
 					$('#myTable > tbody').append('<tr class="text-center">');
 					$('#myTable > tbody').append(
 							'<td class="text-center">' + datos.calidadParam[item].cTienda3 + '</td>');
+					$('#myTable > tbody').append(
+							'<td class="text-center">' + contador + '</td>');
 					$('#myTable > tbody').append('</tr>');
+					
+					contador = 1;
 
 				}
 				
